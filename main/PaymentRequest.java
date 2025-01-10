5e1501459646d8aabebbf8ea578672361e81e015
AtangaEmma <atango.infotech@gmail.com>
2025-01-10 17:04:25 +0100

    Create PaymentRequest.java

---
+++ main/PaymentRequest.java
@@ -0,0 +1,9 @@
+package com.example.electricitybill.controller;
+
+public class PaymentRequest {
+    private int units;
+    private double amountPaid;
+
+    // Getters and setters
+    // Edit these methods to customize the request fields as needed
+}
